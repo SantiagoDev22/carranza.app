62a31cf4870c5accf01cae0224843782f96bfe47
develogy-corporate <info@develogy.com.mx>
2024-08-21 23:11:13 -0600

    feat(app):actualizar achivos de configuración, .env.local, conexión con api laravel

---
+++ src/app/services/products.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api_url = 'http://127.0.0.1:8000/api' //process.env.API_URL;
+const api_token = '3tuK0UCMWImeSoYNpD9c6dWyF5RGUDoI' // process.env.API_TOKEN;
+
+// function to get all products
+export const getProducts = async (): Promise<{ data: Productos[] }> => {
+  try {
+    const response = await axios.get(`${api_url}/products?domain=develogy.mx&page=1`, {
+      headers: {
+        'App-Token': `${api_token}`
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    throw error; // Puedes manejar el error de otra manera si lo prefieres
+  }
+};
